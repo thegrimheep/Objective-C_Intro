c044f5d6b2b6ff8fdbff8d199e209d0fa8095071
David <davidporter33@icloud.com>
2017-04-18 17:17:22 -0700

    Setup the table view to get the employee data to display.

---
+++ Objective-C_Intro/EmployeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmployeeViewController.h
+//  Objective-C_Intro
+//
+//  Created by David Porter on 4/18/17.
+//  Copyright © 2017 David Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmployeeViewController : UIViewController
+
+@end
